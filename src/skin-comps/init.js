067f1633574985de79d063885e3c36e48177e0c7
Evergood <arinmandri@gmail.com>
2025-10-14 23:20:19 +0900

    skin.html 파일 쪼개기

---
+++ src/skin-comps/init.js
@@ -0,0 +1,37 @@
+
+const gt0 = Date.now();
+
+//// url의 파라미터 가져오기
+function getParameters() {
+  let param = new Array();// 파라미터가 담길 배열
+  const url = decodeURIComponent(decodeURIComponent(location.href));// url이 encodeURIComponent 로 인코딩 되었을때는 다시 디코딩 해준다.
+  const params = url.substring(url.indexOf('?') + 1, url.length).split('&');// url에서 '?' 이후의 문자열까지 자르고 '&'로 분리
+  //// params 배열을 '='로 쪼개 param 배열에 key=value 로 담기
+  let key, value;
+  for (let i = 0; i < params.length; i++) {
+    let temp = params[i].split('=')
+    key = temp[0];
+    value = temp[1];
+    param[key] = value;
+  }
+  return param;
+}
+const param = getParameters()
+
+//// 다크모드 - html
+(function(){
+  let isDark = false;
+  const saved = localStorage.getItem('darkmode');
+  if( saved != null ){
+    isDark = saved === 'true' ? true : false;
+  }else{
+    const hour = new Date().getHours();
+    if( hour >= 18 || hour < 6 )
+      isDark = true;
+  }
+  if( isDark ){
+    document.documentElement.setAttribute('data-theme', 'dark');
+  }else{
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+})();
---
+++ src/skin-comps/settings.js
@@ -0,0 +1,179 @@
+
+//// 다크모드 - body
+(function(){
+  const isDark = document.getElementsByTagName('html')[0].dataset.theme === 'dark';
+  if( isDark ){
+    document.body.classList.add('theme-dark');
+    document.body.classList.remove('theme-light');
+  }else{
+    document.body.classList.add('theme-light');
+    document.body.classList.remove('theme-dark');
+  }
+})();
+
+
+
+
+//// 기본값
+const thisHostname = new URL( window.location.href ).hostname;
+
+let noThumbnail = "[##_var_noThumbnail_##]";
+if( noThumbnail === "" )
+  noThumbnail = "./images/noThumbnail-default.svg";
+
+const maxWidthOfMain2        = getOptionValue( '[##_var_maxWidthOfMain2_##]',        1000, 1500 );
+const minWidthToOpenSidebar1 = getOptionValue( '[##_var_minWidthToOpenSidebar1_##]', 1000, 1800 );
+const minWidthToOpenSidebar2 = getOptionValue( '[##_var_minWidthToOpenSidebar2_##]', 1000, 1500 );
+
+function getOptionValue( optionValueString, minVal, defaultVal ) {
+  try{
+    if( optionValueString === '' ) return defaultVal;
+    if( optionValueString == null ) return defaultVal;
+    let val = Number( optionValueString );
+    if( val < minVal ) return defaultVal;
+    return val;
+  }catch (e){
+    console.error('옵션 값 '+optionValueString+'을를 수로 변환  실패');
+    return defaultVal;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+(function(){ 
+  //// 리스트 페이지들 공통 body 클래스 (카테고리 글 리스트, 보관함 글 리스트, 태그 리스트, 검색결과 리스트) (스타일 공통적용 목적)
+  let                 _body = document.getElementById('tt-body-category');
+  if( _body == null ) _body = document.getElementById('tt-body-tag');
+  if( _body == null ) _body = document.getElementById('tt-body-archive');
+  if( _body == null ) _body = document.getElementById('tt-body-search');
+  if( _body == null ) _body = document.getElementById('tt-body-index');
+
+  const body = _body;
+  if( body != null ) body.className = 'tt-body-list';
+
+  //// 설정값
+  /*
+   * settingForm
+   * ├ input.settingCore 설정값 
+   * │    name 속성에 설정값 이름 지정.
+   * │    value 속성에 값 들어감.
+   * │    onchange 에서 설정의 효과 발동.(각자 정의)
+   * │    oninvalid 에서 저장된 설정 없을시의 동작.
+   * ├ .settingAction.toggle 논리값꼴 설정값 // 클릭시 설정값 토글
+   * ├ .settingAction.clickValue 택일
+   * ├ (다른 타입의 설정은 onclick 직접 정의)
+   * ├ input[type="checkbox"].fixCbox 고정 체크박스설정값 있으면 자동으로 체크된다. 고정시 값 저장. 고정 해제시 저장된 값 삭제.
+   * │    oninvalid 에서 체크 해제시의 동작.
+   * └ label.fixCboxLabel 고정 체크박스 라벨
+   */
+  document.addEventListener('DOMContentLoaded', function(){
+    //// 모든 설정요소 가져오기
+    const settingForms = document.getElementsByClassName('settingForm');
+    for( let settingForm of settingForms ){
+
+      const settingCore = settingForm.querySelector('.settingCore');
+      if( settingCore == null ){
+        console.error('settingForm에 name용 태그 없음', settingForm);
+        continue;
+      }
+
+      //// 설정값 이름
+      const settingName = settingCore.name;
+      if( settingName == null || settingName == '' ){
+        console.error('settingFormCore에 name 속성 없음', settingFormCore);
+        continue;
+      }
+      settingCore.id = 'settingCore-' + settingName;
+
+      //// 고정 체크 요소
+      const fixCbox      = settingForm.querySelector('.fixCbox');
+      const fixCboxLabel = settingForm.querySelector('.fixCboxLabel');
+      if( fixCbox != null ){
+        fixCbox.id = 'fixCbox-' + settingName;
+        if( fixCboxLabel != null ){
+          fixCboxLabel.setAttribute( 'for', fixCbox.id );
+        }
+      }
+      fixCbox.addEventListener('change', ()=>{
+        if( checkSettingFixCbox( settingName ) ){
+          localStorage.setItem( settingName, settingCore.value );
+        }else{
+          localStorage.removeItem( settingName );
+          fixCbox.dispatchEvent( new Event('invalid') );
+        }
+      });
+
+      //// 설정값 불러오기
+      const saved = localStorage.getItem( settingName );
+      if( saved != null ){// 설정값 있음
+        //// 고정 체크
+        if( fixCbox != null ){
+          fixCbox.checked = true;
+        }
+        //// 설정값 적용
+        setSettingCore( settingCore, saved );
+      }else{// 설정값 없음: 기본동작
+        settingCore.dispatchEvent( new Event('invalid') );
+      }
+
+      //// 토글버튼
+      const toggleBtns = settingForm.querySelectorAll('.settingAction.toggle');
+      for( let toggleBtn of toggleBtns ){
+        toggleBtn.addEventListener('click', ()=>{
+          if( settingCore.value === 'true' ){
+            setSettingValue0( settingCore, 'false', fixCbox );
+          }else{
+            setSettingValue0( settingCore, 'true', fixCbox );
+          }
+        });
+      }
+
+      //// 클릭하여 값 선택 버튼
+      const clickValueBtns = settingForm.querySelectorAll('.settingAction.clickValue');
+      for( let clickValueBtn of clickValueBtns ){
+        clickValueBtn.addEventListener('click', ()=>{
+          setSettingValue0( settingCore, clickValueBtn.value, fixCbox );
+        });
+      }
+    }
+  });
+})();
+
+function setSettingValue( settingName , value ) {
+  const settingCore = document.getElementById( 'settingCore-' + settingName );
+  const fixCbox     = document.getElementById( 'fixCbox-'     + settingName );
+  setSettingValue0( settingCore, value, fixCbox );
+}
+function setSettingValue0( settingCore , value , fixCbox ) {
+  setSettingCore( settingCore, value );
+  if( fixCbox != null && fixCbox.checked ){
+    localStorage.setItem( settingCore.name, value );
+  }
+}
+
+function setSettingCore( settingCore , value ) {
+  settingCore.value = value;
+  settingCore.dispatchEvent( new Event('change') );
+}
+
+//// 설정값 가져오기
+function getSettingValue( settingName ){
+    return document.getElementById( 'settingCore-' + settingName ).value;
+}
+//// 설정 고정 체크 여부 확인
+function checkSettingFixCbox( settingName ){
+    return document.getElementById( 'fixCbox-' + settingName ).checked;
+}
+//// 설정 고정 체크
+function setSettingFixCbox( settingName, checked ){
+  const fixCbox = document.getElementById( 'fixCbox-' + settingName )
+  fixCbox.checked = checked;
+  fixCbox.dispatchEvent(new Event('change'));
+}
