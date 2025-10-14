fc4e0a763dac5c7e8730ed637ad2c30330a099b9
Evergood <arinmandri@gmail.com>
2025-10-14 20:37:40 +0900

    0
    jiaja 템플릿엔진으로 티스토리 스킨용 파일 생성하기. 현재는 템플릿 문법 전무; 파일 세 개 그대로 복사됨.

---
+++ go.py
@@ -0,0 +1,31 @@
+import os
+import yaml
+from jinja2 import Environment, FileSystemLoader
+
+def load_settings():
+    if os.path.exists("settings.yml"):
+        with open("settings.yml", "r", encoding="utf-8") as f:
+            return yaml.safe_load(f)
+    else:
+        raise FileNotFoundError("settings.yml 파일이 없습니다.")
+
+def make_output_dir_if_not_exists(settings):
+    output_dir = settings["prod"]["dir"]
+    os.makedirs(output_dir, exist_ok=True)
+
+def main():
+    settings = load_settings()
+    make_output_dir_if_not_exists(settings)
+
+    env = Environment(loader=FileSystemLoader("src"))
+
+    for targetFilePath in settings["src"]["targets"]:
+        template = env.get_template(targetFilePath)
+        result = template.render()
+
+        with open(settings["prod"]["dir"] + '/' + targetFilePath, "w", encoding="utf-8") as f:
+            f.write(result)
+
+        print("내보냄: " + targetFilePath)
+
+main()
---
+++ readme.md
@@ -0,0 +1,38 @@
+## 스킨 편집
+
+jinja2 템플릿엔진 이용.
+
+
+
+
+
+## 스킨 적용
+
+### 템플릿 렌더링
+
+`go.py`을 실행하면 산출물이 나온다.
+
+```
+python go.py
+```
+
+ModuleNotFoundError: No module named 뭐뭐 발생시 requirements 설치.
+
+```
+pip install -r requirements.txt
+```
+
+
+
+### 티스토리
+
+산출물을 티스토리 블로그 스킨 등록.
+
+
+
+
+
+## …
+
+[jinja docs](https://jinja.palletsprojects.com/en/stable/)
+
