a67e3e2ccc0725224cf400a784aa39ccf218a305
Evergood <arinmandri@gmail.com>
2025-10-16 17:37:04 +0900

    settings.yml 구조 바꿈

--- go.py
+++ go.py
@@ -10,7 +10,7 @@ def load_settings():
         raise FileNotFoundError("settings.yml 파일이 없습니다.")
 
 def make_output_dir_if_not_exists(settings):
-    output_dir = settings["prod"]["dir"]
+    output_dir = settings["go"]["out_dir"]
     os.makedirs(output_dir, exist_ok=True)
 
 def main():
@@ -19,13 +19,13 @@ def main():
 
     env = Environment(loader=FileSystemLoader("src"))
 
-    for targetFilePath in settings["src"]["targets"]:
+    for targetFilePath in settings["go"]["targets"]:
         template = env.get_template(targetFilePath)
         result = template.render({
             'settings': settings['skin']
         })
 
-        with open(settings["prod"]["dir"] + '/' + targetFilePath, "w", encoding="utf-8") as f:
+        with open(settings["go"]["out_dir"] + '/' + targetFilePath, "w", encoding="utf-8") as f:
             f.write(result)
 
         print("내보냄: " + targetFilePath)
