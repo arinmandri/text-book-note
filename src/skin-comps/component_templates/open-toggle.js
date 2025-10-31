e9ca9614ec7b34790ee91468e44ea1ae6512f8db
Evergood <arinmandri@gmail.com>
2025-10-31 10:32:48 +0900

    검색창 열기 버튼 클릭시 검색 input로 포커스 이동 기능 추가

--- src/skin-comps/component_templates/open-toggle.js
+++ src/skin-comps/component_templates/open-toggle.js
@@ -1,11 +1,15 @@
 //// open 토글버튼
 //// <button class="open-toggleBtn" data-opentarget="대상요소id">OPEN</button>
 //// 대상 요소의 class 중 open을 넣거나 뺀다.
+//// 그 다음 버튼의 onfocus를 실행.
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelectorAll('.open-toggleBtn').forEach(btn => {
     btn.addEventListener('click', () => {
       const target = document.getElementById(btn.dataset.opentarget);
       target.classList.toggle('open');
+      if( btn.onfocus != null ){
+        btn.onfocus();
+      }
     });
   });
 });
