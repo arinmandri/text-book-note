dd17f1fb69f849051ce598605f1700940a895f86
Evergood <arinmandri@gmail.com>
2025-11-03 12:21:29 +0900

    settings에서 폰트의 종류 / 스킨에서 스타일의 종류 구별.
    + fonts: 폰트별 다운 링크와 css 값 정의.(이전 구현에서 skin.fontFamily의 값)
    + skin.font: 앞으로 css에서 참조할, 타입별 이름

--- go.py
+++ go.py
@@ -13,19 +13,29 @@ def make_output_dir_if_not_exists(settings):
     output_dir = settings["go"]["out_dir"]
     os.makedirs(output_dir, exist_ok=True)
 
+def refers_fonts(settings):
+    fonts = settings['fonts']
+    skin_font = settings['skin']['font']
+
+    for fname, ref in skin_font.items():
+        skin_font[fname] = fonts[ref]['cssValue']
+
 def main():
     settings = load_settings()
     make_output_dir_if_not_exists(settings)
 
     env = Environment(loader=FileSystemLoader("src"))
 
+    refers_fonts(settings)
+
     for targetFilePath in settings["go"]["targets"]:
         template = env.get_template(targetFilePath)
         result = template.render({
-            'meta'     : settings['meta'],
             'settings' : settings['skin'],
             'var'      : settings['var'],
-            'headitems': settings['headitems']
+            'headitems': settings['headitems'],
+            'fonts'    : settings['fonts'],
+            'meta'     : settings['meta'],
         })
 
         with open(settings["go"]["out_dir"] + '/' + targetFilePath, "w", encoding="utf-8") as f:
